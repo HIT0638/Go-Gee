0df2ebe1fe81f52f777b6644c9d4d9f269af5aeb
2021111406 <156294753@qq.com>
2023-05-18 17:01:59 +0800

    Day2

--- gee/gee.go
+++ gee/gee.go
@@ -1,32 +1,24 @@
 package gee
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 )
 
 // HandlerFunc defines the request handler used by gee
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(*Context)
 
 // Engine implement the interface of ServeHTTP
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 // New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
-// addRoute is adding route in gee.Engine
-// method: GET, POST, ...
-// pattern: "/", "/xxx", ...
-// handler: the handler function of the route
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	log.Printf("Route %4s - %s", method, pattern)
-	engine.router[key] = handler
+	engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request
@@ -40,20 +32,11 @@ func (engine *Engine) POST(pattern string, handler HandlerFunc) {
 }
 
 // Run defines the method to start a http server
-// addr: the port
 func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
 }
 
-// 这里是 Engine 结构体重写 原net/http 中 Handler的方法
-// 在Go中实现其它方法的结构体可以强转
-// 因此在 Run 中使用 http.ListenAndServe 时可以传入 Engine
-// 从而实现正常的监听以及 Handler 的执行
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
